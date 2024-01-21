b2361f4e46677160593bf353bb4ab54434466d19
Luis Bazaldua <Luis29178@gmail.com>
2024-01-20 21:37:09 -0600

    Enhance News component with detailed content and interactive elements
    - Added a comprehensive event description to provide more context.
    - Implemented a 'Learn More About Events' button with TouchableOpacity for better user interaction.
    - Introduced LinearGradient from expo-linear-gradient for a subtle visual effect on the button.
    - Included additional styling for the new text and button elements, maintaining consistency with existing design.
    - Added a new section for additional news items, preparing the component for future content expansion.
    - Updated scrollView to hide vertical scroll indicator for a cleaner look.
    - Applied shadow styling to the infoButton for a more pronounced and tactile appearance.

--- screens/rewards/Screens/News.tsx
+++ screens/rewards/Screens/News.tsx
@@ -1,7 +1,16 @@
-import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { Colors } from "@/constants/Colors";
 import { Fonts } from "@/constants/Fonts";
+import { Link } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
 
 type propTypes = {
   style: {};
@@ -10,7 +19,7 @@ type propTypes = {
 const News = ({ style }: propTypes) => {
   return (
     <View style={style}>
-      <ScrollView style={styles.scrollView}>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
         <View style={styles.event}>
           <View style={styles.eventInner}>
             <View style={styles.eventTitleContainer}>
@@ -23,12 +32,33 @@ const News = ({ style }: propTypes) => {
 
               <View style={styles.eventImgContainer}>
                 <Image
-                style={styles.eventImg}
+                  style={styles.eventImg}
                   source={require("@/assets/images/dripTheRunway.png")}
                 ></Image>
               </View>
+              <Text style={styles.eventDescription}>
+                {` The year is 2054 and Earth is now uninhabitable. The human species has caused so much damage to the planet’s core it’s slowly deteriorating, and scientists have recently discovered that a huge asteroid is heading directly toward Earth.
+
+  Come with us as we take you on a journey to explore the universe and look for a new planet. What danger awaits us? What is lurking in the great beyond? Is there life on another planet? On our quest, we will be fashionable as space becomes our new runway all the while trying to ensure the survival of our species. There are so many questions that need to be answered. Will we find a new Earth? Are there aliens on this planet? Or are we now the aliens?`}
+              </Text>
+
+              <Link href={"./"} asChild>
+                <TouchableOpacity style={styles.infoButton}>
+                  <Text style={styles.btnText}>Learn More About Events</Text>
+                  <LinearGradient
+                    colors={["transparent", `${Colors.lavenderBlue}21`]}
+                    start={{ x: 0.5, y: 0.35 }}
+                    end={{ x: 0.5, y: 1 }}
+                    style={styles.innerShadow}
+                  />
+                </TouchableOpacity>
+              </Link>
             </View>
           </View>
+        </View>
+        <View style={styles.additionalNews}>
+
+
         </View>
       </ScrollView>
     </View>
@@ -82,19 +112,62 @@ const styles = StyleSheet.create({
   },
 
   eventImgContainer: {
-    marginTop: 10,
+    marginVertical: 10,
     paddingVertical: 10,
     paddingHorizontal: 15,
     width: "100%",
     height: 300,
     alignContent: "center",
     justifyContent: "center",
-
-
   },
   eventImg: {
     width: "100%",
     height: 300,
     borderRadius: 15,
-  }
+  },
+
+  eventDescription: {
+    fontSize: 16,
+    fontFamily: Fonts.body,
+    color: Colors.heavyGray,
+    marginBottom: 10,
+  },
+
+  infoButton: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    backgroundColor: Colors.white,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+
+    elevation: 2,
+
+    borderColor: Colors.primary,
+    borderRadius: 9001,
+  },
+
+  btnText: {
+    color: Colors.primary,
+    fontFamily: Fonts.button,
+    fontSize: 16,
+  },
+  innerShadow: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    borderRadius: 9001,
+  },
+  additionalNews: {
+    height: 300,
+    width: "100%"
+  },
 });
