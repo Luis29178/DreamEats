89da4579483bc4a0e3bd615389455ce36e682694
Luis Bazaldua <Luis29178@gmail.com>
2024-01-20 18:52:08 -0600

    .

--- screens/rewards/Screens/News.tsx
+++ screens/rewards/Screens/News.tsx
@@ -1,14 +1,100 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { Colors } from "@/constants/Colors";
+import { Fonts } from "@/constants/Fonts";
 
 type propTypes = {
   style: {};
 };
 
 const News = ({ style }: propTypes) => {
-  return <View style={style}></View>;
+  return (
+    <View style={style}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.event}>
+          <View style={styles.eventInner}>
+            <View style={styles.eventTitleContainer}>
+              <Text style={styles.eventTitle}>{`Latest Events`}</Text>
+              <Text style={styles.eventSpacer}>{`···`}</Text>
+              <Text style={styles.eventName}>{`Drip The Runway`}</Text>
+              <Text
+                style={styles.eventDate}
+              >{`Saturday, July 22 · 7pm EST`}</Text>
+
+              <View style={styles.eventImgContainer}>
+                <Image
+                style={styles.eventImg}
+                  source={require("@/assets/images/dripTheRunway.png")}
+                ></Image>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default News;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
+  event: {
+    width: "100%",
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderColor: Colors.primary,
+  },
+  eventInner: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.lighterGray,
+  },
+
+  eventTitleContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  eventTitle: {
+    fontSize: 40,
+    fontFamily: Fonts.title,
+    color: Colors.heavyGray,
+    textAlign: "center",
+  },
+  eventSpacer: {
+    fontSize: 36,
+    fontFamily: Fonts.title,
+    color: Colors.heavyGray,
+  },
+  eventName: {
+    fontSize: 26,
+    fontFamily: Fonts.title,
+    color: Colors.heavyGray,
+  },
+  eventDate: {
+    fontSize: 12,
+    fontFamily: Fonts.title,
+    color: Colors.heavyGray,
+  },
+
+  eventImgContainer: {
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    width: "100%",
+    height: 300,
+    alignContent: "center",
+    justifyContent: "center",
+
+
+  },
+  eventImg: {
+    width: "100%",
+    height: 300,
+    borderRadius: 15,
+  }
+});
